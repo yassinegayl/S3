51115e34ac4d11c64b5de2dfa145b5d6650e13dd
Jonathan Gramain <jonathan.gramain@scality.com>
2017-04-11 15:06:26 -0700

    S3C-158 a few config-related changes
     - simplify data daemon config (no default value allowed)
     - move S3DATAPATH env var usage in the dataserver.js file instead of the global Config.js
     - create a startup rule that starts both data and metadata servers, but not S3 server (aka. dmd rule)

--- dataserver.js
+++ dataserver.js
@@ -6,11 +6,17 @@ const config = require('./lib/Config.js').default;
 const logger = require('./lib/utilities/logger').logger;
 
 if (config.backends.data === 'file') {
+    /**
+     * Configure the file paths for data if using the file
+     * backend. If no path provided, uses data at the root of
+     * the S3 project directory.
+     */
+    const dataPath = process.env.S3DATAPATH ?
+              process.env.S3DATAPATH : `${__dirname}/localData`;
     const restServer = new arsenal.network.rest.RESTServer(
         { port: config.dataDaemon.port,
           dataStore: new arsenal.storage.data.file.DataFileStore(
-              { dataPath: config.dataDaemon.dataPath,
-                log: config.log }),
+              { dataPath, log: config.log }),
           log: config.log });
     restServer.setup(err => {
         if (err) {
