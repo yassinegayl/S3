c42651a306c029e5c0c454d883cc811322aa8665
Nicolas Humbert <nicolas.humbert@live.fr>
2017-09-08 14:50:04 -0700

    adding tests

--- tests/functional/aws-node-sdk/test/multipleBackend/objectPutCopyPart/objectPutCopyPartAzure.js
+++ tests/functional/aws-node-sdk/test/multipleBackend/objectPutCopyPart/objectPutCopyPartAzure.js
@@ -1,8 +1,11 @@
 const async = require('async');
 const assert = require('assert');
+const { s3middleware } = require('arsenal');
+const azureMpuUtils = s3middleware.azureHelper.mpuUtils;
 
 const { config } = require('../../../../../../lib/Config');
 const authdata = require('../../../../../../conf/authdata.json');
+const constants = require('../../../../../../constants');
 const withV4 = require('../../support/withV4');
 const BucketUtility = require('../../../lib/utility/bucket-util');
 const { uniqName, getAzureClient } = require('../utils');
@@ -22,6 +25,9 @@ config.locationConstraints[azureLocation].details.azureContainerName) {
 
 const normalBody = Buffer.from('I am a body', 'utf8');
 const normalMD5 = 'be747eb4b75517bf6b3cf7c5fbb62f3a';
+
+const hundredKbBody = Buffer.alloc(1000);
+const hundredKbMD5 = 'ede3d3b685b4e137ba4cb2521329a75e';
 
 const keyObject = 'objectputcopypartAzure';
 const azureClient = getAzureClient();
@@ -101,7 +107,7 @@ describeSkipIfNotMultiple('Put Copy Part to AZURE', () => {
                 };
                 s3.abortMultipartUpload(params, done);
             });
-            it.only('should copy part from Azure to Azure', function ifF(done) {
+            it('should copy part from Azure to Azure', function ifF(done) {
                 const params = {
                     Bucket: azureContainerName,
                     CopySource:
@@ -135,7 +141,92 @@ describeSkipIfNotMultiple('Put Copy Part to AZURE', () => {
                         assert.deepStrictEqual(res, this.test.resultCopy);
                         next();
                     }),
+                    next => azureClient.listBlocks(azureContainerName,
+                    this.test.mpuKeyName, 'all', (err, res) => {
+                        assert.equal(err, null, 'listBlocks: Expected ' +
+                        `success, got error: ${err}`);
+                        const partName = azureMpuUtils.getBlockId(
+                          this.test.uploadId, 1, 0);
+                        assert.strictEqual(res.UncommittedBlocks[0].Name,
+                          partName);
+                        assert.equal(res.UncommittedBlocks[0].Size, 11);
+                        next();
+                    }),
                 ], done);
+            });
+
+            describe('with existing part', () => {
+                beforeEach(function beF(done) {
+                    const params = {
+                        Body: hundredKbBody,
+                        Bucket: azureContainerName,
+                        Key: this.currentTest.mpuKeyName,
+                        PartNumber: 1,
+                        UploadId: this.currentTest.uploadId,
+                    };
+                    s3.uploadPart(params, done);
+                });
+                it.only('should copy part from Azure to Azure with existing ' +
+                'parts', function ifF(done) {
+                    const params = {
+                        Bucket: azureContainerName,
+                        CopySource:
+                          `${azureContainerName}/${this.test.keyName}`,
+                        Key: this.test.mpuKeyName,
+                        PartNumber: 2,
+                        UploadId: this.test.uploadId,
+                    };
+                    async.waterfall([
+                        next => s3.uploadPartCopy(params, (err, res) => {
+                            assert.equal(err, null,
+                              'uploadPartCopy: Expected success, got ' +
+                              `error: ${err}`);
+                            assert.strictEqual(res.ETag, `"${normalMD5}"`);
+                            next(err);
+                        }),
+                        next => s3.listParts({
+                            Bucket: azureContainerName,
+                            Key: this.test.mpuKeyName,
+                            UploadId: this.test.uploadId,
+                        }, (err, res) => {
+                            assert.equal(err, null, 'listParts: Expected ' +
+                            `success, got error: ${err}`);
+                            this.test.resultCopy.Bucket = azureContainerName;
+                            this.test.resultCopy.Key = this.test.mpuKeyName;
+                            this.test.resultCopy.UploadId = this.test.uploadId;
+                            this.test.resultCopy.Parts =
+                             [{ PartNumber: 1,
+                                 LastModified: res.Parts[0].LastModified,
+                                 ETag: `"${hundredKbMD5}"`,
+                                 Size: 1000 },
+                               { PartNumber: 2,
+                                   LastModified: res.Parts[1].LastModified,
+                                   ETag: `"${normalMD5}"`,
+                                   Size: 11 },
+                             ];
+                            assert.deepStrictEqual(res, this.test.resultCopy);
+                            next();
+                        }),
+                        next => azureClient.listBlocks(azureContainerName,
+                        this.test.mpuKeyName, 'all', (err, res) => {
+                            assert.equal(err, null, 'listBlocks: Expected ' +
+                            `success, got error: ${err}`);
+                            const partName = azureMpuUtils.getBlockId(
+                              this.test.uploadId, 1, 0);
+                            const partName2 = azureMpuUtils.getBlockId(
+                              this.test.uploadId, 2, 0);
+                            assert.strictEqual(res.UncommittedBlocks[0].Name,
+                              partName);
+                            assert.equal(res.UncommittedBlocks[0].Size,
+                            1000);
+                            assert.strictEqual(res.UncommittedBlocks[1].Name,
+                                partName2);
+                            assert.equal(res.UncommittedBlocks[1].Size,
+                            11);
+                            next();
+                        }),
+                    ], done);
+                });
             });
         });
     });
