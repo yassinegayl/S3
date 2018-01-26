bff843643a7a09969ece334d886db337b1d44823
Akim Sadaoui <akim.sadaoui@scality.com>
2018-01-26 14:37:53 +0100

    disable md and dataserver

--- toto.sh
+++ toto.sh
@@ -12,26 +12,26 @@ unset AZURE_STORAGE_ACCOUNT
 unset AZURE_STORAGE_ACCESS_KEY
 
 
-node mdserver.js > mdserver.out 2>&1 &
-node dataserver.js > dataserver.out 2>&1 &
-while true
-do nc -w 1 localhost 9991
-    ret=$?
-    echo "ret: $ret"
-    if [ "$ret" -eq 0 ]
-    then
-        break
-    fi
-    sleep 0.5
-done
-while true
-do nc -w 1 localhost 9990
-    ret=$?
-    echo "ret: $ret"
-    if [ "$ret" -eq 0 ]
-    then
-        break
-    fi
-    sleep 0.5
-done
+# node mdserver.js > mdserver.out 2>&1 &
+# node dataserver.js > dataserver.out 2>&1 &
+# while true
+# do nc -w 1 localhost 9991
+#     ret=$?
+#     echo "ret: $ret"
+#     if [ "$ret" -eq 0 ]
+#     then
+#         break
+#     fi
+#     sleep 0.5
+# done
+# while true
+# do nc -w 1 localhost 9990
+#     ret=$?
+#     echo "ret: $ret"
+#     if [ "$ret" -eq 0 ]
+#     then
+#         break
+#     fi
+#     sleep 0.5
+# done
 npm run multiple_backend_test
